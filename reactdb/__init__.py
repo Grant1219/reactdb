67142fbd56ade2b92996158c8d1635e0f822f36e
Grant <grant@tuxinator.net>
2012-06-26 23:55:00 -0400

    Setup based database structure.

--- reactdb/__init__.py
+++ reactdb/__init__.py
@@ -1,13 +1,13 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from models import DBSession
+from models import initialize_db
 
 def main (global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     engine = engine_from_config (settings, 'sqlalchemy.')
-    DBSession.configure (bind = engine)
+    initialize_db (engine)
 
     config = Configurator (settings = settings)
 
--- reactdb/models.py
+++ reactdb/models.py
@@ -1,7 +1,10 @@
 from sqlalchemy import (
+    Table,
     Column,
     Integer,
-    Text,
+    BigInteger,
+    Unicode,
+    ForeignKey
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,9 +12,41 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
+    backref,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
+DBSession = scoped_session (sessionmaker (extension=ZopeTransactionExtension () ) )
+Base = declarative_base ()
+
+image_keyword_association = Table ('image_keyword_association', Base.metadata,
+    Column ('image_id', BigInteger, ForeignKey ('image.id', ondelete = 'cascade') ),
+    Column ('keyword_id', BigInteger, ForeignKey ('keyword.id', ondelete = 'cascade') ),
+    mysql_engine = 'InnoDB'
+)
+
+class Image (Base):
+    __tablename__ = 'image'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+
+    id = Column (BigInteger, primary_key = True)
+    filename = Column (Unicode (30), unique = True)
+
+    keywords = relationship ('Keyword', secondary = image_keyword_association, backref = 'images')
+
+    def __init__ (self, filename):
+        self.filename = filename
+
+class Keyword (Base):
+    __tablename__ = 'keyword'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+
+    id = Column (BigInteger, primary_key = True)
+    value = Column (Unicode (30), unique = True)
+
+def initialize_db (engine):
+    DBSession.configure (bind = engine)
+    Base.metadata.bind = engine
+    Base.metadata.create_all (engine)
